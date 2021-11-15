96e6c1e3d69d1b318b27f3bbf28689a3ec058cb2
Raphael D. Pinheiro <raphaklaus@gmail.com>
2021-11-15 18:23:03 +0100

    Use multi to leverage db transactions

--- lib/naive_dice/events/events.ex
+++ lib/naive_dice/events/events.ex
@@ -1,6 +1,8 @@
 defmodule NaiveDice.Events do
+  import Ecto.Query
   alias NaiveDice.Events.{Ticket, Event}
   alias NaiveDice.Repo
+  alias Ecto.Multi
 
   @spec list_all :: list(%Event{})
   def list_all do
@@ -18,14 +20,44 @@ defmodule NaiveDice.Events do
     end
   end
 
+  # My TODO: Separate functions of ticket and event into different modules
+  def get_by_payment_id(id) do
+    Repo.get_by(Ticket, payment_id: id)
+  end
+
+  def confirm_paid_ticket(ticket) do
+    Multi.new()
+    |> Multi.run(:check_available_tickets, fn repo, _ -> has_available_tickets?(repo, ticket.event) end)
+    |> Multi.update(:ticket, Ticket.changeset(ticket, %{confirmed: true}))
+    |> Repo.transaction()
+    |> IO.inspect
+  end
+
   def new_ticket_changeset(event) do
     Ticket.changeset(%Ticket{}, %{event: event})
   end
 
-  def reserve_ticket(event, user_name) do
-    %Ticket{}
-    |> Ticket.changeset(%{user_name: user_name, event: event})
-    |> Repo.insert()
+  def has_available_tickets?(repo, event) do
+    allocation = from(
+      t in Ticket,
+      where: t.event_id == ^event.id and t.confirmed == true,
+      select: count(t.id)
+    ) |> repo.one()
+
+    IO.inspect allocation
+
+    if allocation >= 5, do: {:error, allocation}, else: {:ok, allocation}
+  end
+
+  def reserve_ticket(_event, _user_name, nil) do
+    {:error, :no_payment_id}
+  end
+
+  def reserve_ticket(event, user_name, payment_id) do
+    Multi.new()
+    |> Multi.run(:check_available_tickets, fn repo, _ -> has_available_tickets?(repo, event) end)
+    |> Multi.insert(:create_ticket, Ticket.changeset(%Ticket{}, %{user_name: user_name, event: event, payment_id: payment_id}))
+    |> Repo.transaction()
   end
 
   def get_ticket_by_id(ticket_id) do
